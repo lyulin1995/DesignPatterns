726c5e65c5cc512e2bc44af7c477ffb40a15f6e7
Lin Lyu <lyulin.lynn@gmail.com>
2021-10-18 20:30:04 +0200

    Initial commit

---
+++ src/com/company/User.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class User {
+    // Fields (attributes)
+    public String name;
+
+    public User (String name) {
+        // Constructor: a method that gets called when we create a new instance of a class.
+        this.name = name;
+        // if we type name = name, the java compiler will think we are referring to this name parameter.
+        // Using this. to access all the fields and methods in this class.
+        
+    }
+    // Methods
+
+}
